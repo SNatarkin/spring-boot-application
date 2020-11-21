22bd0e5d7dd124b03905c23ba835fdadfae7734c
“Natarkin <snatarkina@mail.ru>
2020-11-21 23:23:01 +0300

    add Student class

--- src/main/java/springbootapp/model/Student.java
+++ src/main/java/springbootapp/model/Student.java
@@ -4,16 +4,22 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-
+import javax.validation.constraints.NotBlank;
 import java.util.UUID;
 
 @AllArgsConstructor
 public class Student {
+
     @Getter
-    @JsonProperty("id")
     private final UUID id;
+    /**
+     * JsonProperty("id") - this one says that the given attribute in JSON will be named as Name
+     *
+     * @Notblack - put it so that there are no empty and null values and just spaces
+     */
     @Getter
-    @JsonProperty("studentName")
+    @JsonProperty("Name")
+    @NotBlank
     private final String studentName;
 
 
